3f295699d176abb71146a38718f1a53710cb5ce2
SapphireClawe <37085710+SapphireClawe@users.noreply.github.com>
2018-11-02 11:49:49 -0600

    v

--- Fireworks/Firework.java
+++ Fireworks/Firework.java
@@ -8,13 +8,13 @@ class Firework () {
   float gravity;
 
 
-  Firework (float x, float y, diameter, float xSpeed, float ySpeed, float gravity) {
+  Firework (float x, float y, diameter) {
     this.x = x;
     this.y = y;
     this.c = color( int(random(255)), int(random(255)), int(random(255)));
     this.diameter = random(diameter*1/50);
-    this.xSpeed = xSpeed;
-    this.ySpeed = ySpeed;
-    this.gravity = gravity;
+    this.xSpeed = random(-5, 5);
+    this.ySpeed = random(-5, 5);
+    this.gravity = 0.5;
   }
 }
